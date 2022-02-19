1479560fd7d8749f36b0e70d280f199924437160
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-02-19 05:24:27 -0500

    Finished navbar

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <Dashnav/>
+</template>
+<script>
+import styles from "@/assets/styles.json"
+import Dashnav from "@/components/Dashnav.vue"
+export default {
+    name: "Dashbaord",
+    components: {
+        Dashnav
+    }
+}
+</script>
