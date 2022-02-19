c849656727b7c54e439e7ad7b17d08be8d95b947
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-02-19 18:39:37 -0500

    Started trans

--- frontend/src/components/Dashnav.vue
+++ frontend/src/components/Dashnav.vue
@@ -1,5 +1,5 @@
 <template>
-    <main :style="{'width': '300px'}">
+    <main :style="{'width': '300px', 'background': styles.navbar[getMode]}">
         <h1>{{styles.name}}</h1>
         <div class="item" :style="[tab[0] == 1 ? {'background-color': 'rgba(255, 255, 255, 0.322)'} : none]" @click="changeTab(0)">
             <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 12c1.65 0 3-1.35 3-3s-1.35-3-3-3-3 1.35-3 3 1.35 3 3 3zm0-4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm6 8.58c0-2.5-3.97-3.58-6-3.58s-6 1.08-6 3.58V18h12v-1.42zM8.48 16c.74-.51 2.23-1 3.52-1s2.78.49 3.52 1H8.48zM19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14z"/></svg>
@@ -21,7 +21,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 9c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm0 8.55C9.64 9.35 6.48 8 3 8v11c3.48 0 6.64 1.35 9 3.55 2.36-2.19 5.52-3.55 9-3.55V8c-3.48 0-6.64 1.35-9 3.55zm7 5.58c-2.53.34-4.93 1.3-7 2.82-2.06-1.52-4.47-2.49-7-2.83v-6.95c2.1.38 4.05 1.35 5.64 2.83L12 14.28l1.36-1.27c1.59-1.48 3.54-2.45 5.64-2.83v6.95z"/></svg>
             <p>Learn</p>
         </div>
-        <button class="container" aria-label="Toggle color mode" title="Toggle color mode" @click="">
+        <button class="container" aria-label="Toggle color mode" title="Toggle color mode" @click="toggleDark()">
             <div class="sun visible"></div>
             <div class="moon">
                 <div class="star"></div>
@@ -45,16 +45,22 @@ export default {
     data(){
         return{
             styles: styles,
-            tab: [1,0,0,0,0]
+            dark: this.$store.state.dark,
+            tab: [1,0,0,0,0],
+            none: ""
+        }
+    },
+    computed:{
+        getMode(){
+            return this.$store.state.dark;
         }
     },
     methods:{
         changeTab(index){
-            this.tab[0] = 0;
-            this.tab[1] = 0;
-            this.tab[2] = 0;
-            this.tab[3] = 0;
-            this.tab[4] = 0;
+           
+            this.tab.forEach((item, index, array) => {
+                array[index] = 0;
+            });
             this.tab[index] = 1;
             if(index == 0){
                 this.$router.push("/dashboard");
@@ -70,6 +76,11 @@ export default {
         },
         logout(){
             this.$router.push("/");
+        },
+        toggleDark(){
+            this.$store.commit("changeMode");
+            this.dark = this.$store.state.dark;
+     
         }
     },
     mounted(){
@@ -82,6 +93,7 @@ export default {
             moon.classList.toggle('visible');
         })
         this.tab = this.$store.state.tabs;
+        console.log("MOUTNED WHEN CHANGE");
 
     }
 }
@@ -95,8 +107,8 @@ main{
     height: 100vh;
     // background: #42b983;
     position: relative;
-    background: rgb(80,213,183);
-    background: radial-gradient(circle, rgba(80,213,183,1) 0%, rgba(66,185,131,1) 100%);
+    // background: rgb(80,213,183);
+    // background: radial-gradient(circle, rgba(80,213,183,1) 0%, rgba(66,185,131,1) 100%);
     float: left;
 }
 h1{
@@ -116,6 +128,7 @@ h1{
     padding: 10px 0px 10px 20px;
     border-top-left-radius: 20px;
     border-bottom-left-radius: 20px;
+    user-select: none;
 
     &:hover{
         cursor: pointer;
--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -1,12 +1,14 @@
 <template>
-    <main class="main">
-        <h1>Hello, Jax Wang</h1>
+    <main class="main" :style="{'background-color': styles.bgcolour[getMode]}">
+        <h1 :style="{'color': styles.headertext[getMode]}">Hello, Jax Wang!</h1>
         <section class="head">
             <div class="head__balance">
+                <h2 class="head__balance--title">Balance</h2>
                 <h2 class="head__balance--amount">$100,000</h2>
             </div>
-
+            
         </section>
+        <section class="transaction"></section>
     </main>
 </template>
 <script>
@@ -17,11 +19,20 @@ export default {
     
     data(){
         return{
-            navwidth: "300px"
+            styles: styles,
+            navwidth: "300px",
+            dark: this.$store.state.dark,
+        }
+        
+    },
+    computed:{
+        getMode(){
+            return this.$store.state.dark;
         }
     },
     mounted(){
         this.$store.commit("changeTab", 0);
+        this.dark = this.$store.state.dark;
     }
 }
 </script>
@@ -35,7 +46,8 @@ export default {
     width: calc(100% - 300px);
     & > h1{
         text-align: left;
-        padding: 30px 50px;
+        padding: 30px 100px;
+        font-size: 50px;
     }
 }
 .head{
@@ -43,15 +55,26 @@ export default {
     flex-direction: row;
 
     &__balance{
-        height: 200px;
+        height: 150px;
         width: 400px;
         background: rgb(80,213,183);
         background: linear-gradient(338deg, rgba(80,213,183,1) 0%, rgba(66,185,131,1) 100%);
         margin-left: 100px;
         border-radius: 20px;
         &--amount{
+            font-size: 40px;
+            color: white;
+            text-align: left;
+            padding: 0px 0px 0px 30px;
+            font-weight: 500;
+        }
+        &--title{
             font-size: 30px;
             color: white;
+            padding: 20px 0px 0px 30px;
+            text-align: left;
+            font-weight: 200;
+
         }
 
     }
